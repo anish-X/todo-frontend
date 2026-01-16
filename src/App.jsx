68a0a87e6825101404dbc39d64bf9285e38de478
anish-X <anish.stha527@gmail.com>
2026-01-16 12:10:39 +0545

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
   return (
     <div className="app">
       <div className="container">
-        <h1>Todo dfsdfssdfsdfdsp</h1>
+        <h1>Todo dfsdfssdfsdfdp</h1>
         <TodoForm onAdd={addTodo} />
         <TodoList todos={todos} onDelete={deleteTodo} onToggle={toggleTodo} />
       </div>
