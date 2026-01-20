31413ecb2e34a70ba61438d74b7253f97f078205
anish-X <anish.stha527@gmail.com>
2026-01-20 12:51:05 +0545

    asdf

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -13,7 +13,7 @@ function TodoItem({ todo, onDelete, onToggle }) {
         className="delete-button"
         aria-label="Delete todo"
       >
-        Deleteeea
+        Deleteee
       </button>
     </li>
   );
