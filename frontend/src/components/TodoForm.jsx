689d4d30bfded94edb716701139f13ced1287afb
anish-X <anish.stha527@gmail.com>
2026-01-22 16:27:41 +0545

    asdf

---
+++ frontend/src/components/TodoForm.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function TodoForm({ onAdd }) {
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAdd(input);
+    setInput("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="todo-form">
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Add a new todo....."
+        className="todo-input"
+      />
+      <button type="submit" className="todo-button">
+        Add Todo
+      </button>
+    </form>
+  );
+}
+
+export default TodoForm;
